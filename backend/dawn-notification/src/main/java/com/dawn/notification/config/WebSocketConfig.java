b931005eb3acf8a5546963dca58d94df81ce06a8
DawnBreaker <tunganhngo207@gmail.com>
2026-05-25 13:48:25 +0700

    feat: update notification module

--- backend/dawn-notification/src/main/java/com/dawn/notification/config/WebSocketConfig.java
+++ backend/dawn-notification/src/main/java/com/dawn/notification/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.dawn.common.infra.websocket;
+package com.dawn.notification.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
